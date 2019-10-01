8dbefb707c8f87d2cd4adf2916fc2ab97b40429f
oraz <oraz.ra@gmail.com>
2019-10-01 15:39:20 +0200

    tests

---
+++ app/test-utils/random-utils.ts
@@ -0,0 +1,13 @@
+const charactersAndNumbers =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+const charactersAndNumbersLength = charactersAndNumbers.length;
+
+export function randomAlphanumeric(length = 10): string {
+  let result = '';
+  for (let i = 0; i < length; i++) {
+    result += charactersAndNumbers.charAt(
+      Math.floor(Math.random() * charactersAndNumbersLength)
+    );
+  }
+  return result;
+}
