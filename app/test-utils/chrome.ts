e7569c7f2c3eb63a82860c7bda1cdd54f9d2853d
oraz <oraz.ra@gmail.com>
2019-10-01 16:41:52 +0200

    testing

--- app/test-utils/chrome.ts
+++ app/test-utils/chrome.ts
@@ -15,8 +15,34 @@ class TabsApi {
   update(tabsId: number, updateProperties: { url?: string }): void {
     throw Error('Not implemented!');
   }
+
+  create(createProperties: {
+    windowId?: number;
+    index?: number;
+    url?: string;
+    active?: boolean;
+    pinned?: boolean;
+    openerTabId?: number;
+  }) {
+    throw Error('Not implemented!');
+  }
 }
 
+class WindowsApi {
+  create(createData?: {
+    url?: string | string[];
+    tabId?: number;
+    left?: number;
+    top?: number;
+    width?: number;
+    height?: number;
+    focused?: boolean;
+    incognito?: boolean;
+    setSelfAsOpener?: boolean;
+  }) {
+    throw Error('Not implemented!');
+  }
+}
 class BookmarksApi {
   private getTreeCallback: (nodes: BookmarkTreeNode[]) => void = nodes => {
     throw Error('Not implemented!');
@@ -35,6 +61,7 @@ export class Chrome {
   i18n: I18NApi;
   tabs: TabsApi;
   bookmarks: BookmarksApi;
+  windows: WindowsApi;
 
   constructor() {
     this.reset();
@@ -44,5 +71,6 @@ export class Chrome {
     this.i18n = new I18NApi();
     this.tabs = new TabsApi();
     this.bookmarks = new BookmarksApi();
+    this.windows = new WindowsApi();
   }
 }
