e264c79a3b39c619fc3b1536ce499dacc53e85cc
Roman Orazmagomedov <oraz.ra@gmail.com>
2019-10-02 21:21:05 +0200

    fixed css for google bookmarks error message

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,24 @@
 module.exports = {
-    printWidth: 80,
-    tabWidth: 2,
-    useTabs: false,
-    semi: true,
-    singleQuote: true,
-    trailingComma: 'none',
-    bracketSpacing: true,
-    jsxBracketSameLine: false,
-    arrowParens: 'avoid',
-    rangeStart: 0,
-    rangeEnd: Infinity,
-    requirePragma: false,
-    insertPragma: false,
-    proseWrap: 'preserve'
-  };
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  trailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: 'avoid',
+  rangeStart: 0,
+  rangeEnd: Infinity,
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  overrides: [
+    {
+      files: '**/*.css',
+      options: {
+        singleQuote: false
+      }
+    }
+  ]
+};
