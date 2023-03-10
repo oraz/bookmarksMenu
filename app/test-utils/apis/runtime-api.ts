b0ed8d59d0f17d50b76eb120e9aec0c3d503d7e3
Roman Orazmagomedov <oraz.ra@gmail.com>
2023-03-10 21:40:03 +0100

    refactor(chrome-mocks, #61): refactor chrome-mocks (#64)

--- app/test-utils/apis/runtime-api.ts
+++
@@ -1,9 +0,0 @@
-/**
- * @see https://developer.chrome.com/docs/extensions/reference/runtime/
- */
-
-export class RuntimeApiImpl {
-    getURL(url: string): string {
-        return 'file://../../icons/folder-win.png';
-    }
-}
